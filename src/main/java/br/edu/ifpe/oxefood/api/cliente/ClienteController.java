76c77d33d241a054fa39496ec4e0818368c0dfb0
romulo2639 <roargs2009@gmail.com>
2026-09-24 20:34:02 -0300

    Realizando Alteração

--- src/main/java/br/edu/ifpe/oxefood/api/cliente/ClienteController.java
+++ src/main/java/br/edu/ifpe/oxefood/api/cliente/ClienteController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,27 +18,33 @@ public class ClienteController {
 
     private final ClienteService clienteService;
 
-    public ClienteController(ClienteService service) {
-        this.clienteService = service;
+    public ClienteController(ClienteService clienteService) {
+        this.clienteService = clienteService;
     }
 
     @PostMapping
     public ResponseEntity<Cliente> cadastrar(@RequestBody ClienteDTO dto) {
-
         Cliente clienteCadastrado = clienteService.cadastrar(dto);
-        return ResponseEntity.status(HttpStatus.CREATED).body(clienteCadastrado);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(clienteCadastrado);
     }
 
     @GetMapping
     public ResponseEntity<List<Cliente>> listar() {
-
         return ResponseEntity.ok(clienteService.listar());
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Cliente> buscarPorId(@PathVariable Long id) {
-
         return ResponseEntity.ok(clienteService.buscarPorId(id));
     }
 
+    @PutMapping
+    public ResponseEntity<Cliente> atualizar(@RequestBody ClienteDTO dto) {
+        Cliente clienteAtualizado = clienteService.atualizar(dto);
+
+        return ResponseEntity.ok(clienteAtualizado);
+    }
 }
--- src/main/java/br/edu/ifpe/oxefood/api/cliente/ClienteService.java
+++ src/main/java/br/edu/ifpe/oxefood/api/cliente/ClienteService.java
@@ -3,6 +3,7 @@ package br.edu.ifpe.oxefood.api.cliente;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+
 import jakarta.transaction.Transactional;
 
 @Service
@@ -11,12 +12,22 @@ public class ClienteService {
     private final ClienteRepository repository;
 
     public ClienteService(ClienteRepository repository) {
-       this.repository = repository;
+        this.repository = repository;
     }
 
     public Cliente build(ClienteDTO dto) {
 
-        Cliente cliente = new Cliente();
+        Cliente cliente = null;
+
+        if (dto.getId() == null) { // Montado para o cadastro
+
+            cliente = new Cliente();
+
+        } else { // Consultado para a alteração
+
+            cliente = repository.findById(dto.getId()).get();
+        }
+
         cliente.setNome(dto.getNome());
         cliente.setDataNascimento(dto.getDataNascimento());
         cliente.setCpf(dto.getCpf());
@@ -31,17 +42,25 @@ public class ClienteService {
 
         Cliente cliente = build(dto);
         cliente.setHabilitado(true);
+
         return repository.save(cliente);
     }
 
     public List<Cliente> listar() {
 
-        return repository.findAll(); //SELECT * FROM Cliente
+        return repository.findAll();
     }
 
     public Cliente buscarPorId(Long id) {
 
-        return repository.findById(id).get();   //SELECT * FROM Cliente WHERE id = ?
+        return repository.findById(id).get();
     }
 
+    @Transactional
+    public Cliente atualizar(ClienteDTO dto) {
+
+        Cliente cliente = build(dto);
+
+        return repository.save(cliente);
+    }
 }
